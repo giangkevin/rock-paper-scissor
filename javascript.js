b55cb8d7ff4c49dd52e7119138ea7698cd19bcfb
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-08-16 19:29:52 +0200

    Add getHumanChoice()

--- javascript.js
+++ javascript.js
@@ -3,3 +3,9 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+function getHumanChoice(){
+    let answer = prompt("Rock, paper or scissor ?");
+    console.log(answer);
+}
+
+getHumanChoice();
