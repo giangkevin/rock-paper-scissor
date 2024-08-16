5667e39728b91ca0b4d5e13dacf9dfad9a57e989
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-08-16 19:41:04 +0200

    Add playround()

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissor'];
     return choices[Math.floor(Math.random() * 3)];
@@ -5,7 +8,42 @@ function getComputerChoice() {
 
 function getHumanChoice(){
     let answer = prompt("Rock, paper or scissor ?");
-    console.log(answer);
+    return answer.toLowerCase();
 }
 
-getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            console.log("You lose! " + humanChoice + " lose to " + computerChoice); 
+            computerScore += 1;
+        } else if (computerChoice === 'scissor') {
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+            humanScore += 1;
+        }else if (humanChoice === computerChoice){
+            console.log("You both picked " + humanChoice);
+        }
+    } else if (humanChoice === 'paper') {
+        if (computerChoice === 'scissor') {
+            console.log("You lose! " + humanChoice + " lose to " + computerChoice); 
+            computerScore += 1;
+        } else if (computerChoice === 'rock') {
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+            humanScore += 1;
+        }else if (humanChoice === computerChoice){
+            console.log("You both picked " + humanChoice);
+        }
+    } else if (humanChoice === 'scissor') {
+        if (computerChoice === 'rock') {
+            console.log("You lose! " + humanChoice + " lose to " + computerChoice); 
+            computerScore += 1;
+        } else if (computerChoice === 'paper') {
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+            humanScore += 1;
+        }else if (humanChoice === computerChoice){
+            console.log("You both picked " + humanChoice);
+        }
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
